b8514dea509f768a751080d3a78c517572a422e0
Ganesh Katrapati <neshmailsu@gmail.com>
2025-06-24 21:57:21 +0530

    YAML file

--- generate_source.py
+++ generate_source.py
@@ -1,20 +1,54 @@
 import sys
+import re
 
-src_file = sys.argv[1]
+inp_file = sys.argv[1]
+src_file = sys.argv[2]
+gen_file = sys.argv[3]
+mode = sys.argv[4]
 
-vmap = {
-    "jump" : "I_JUMP",
-    "look" : "I_LOOK",
-    "walk" : "I_WALK",
-    "run" : "I_RUN"
-}
+tups = re.compile("\((.*?)\)+")
 
-with open(src_file) as s:
+with open(inp_file) as ip, open(src_file) as s, open(gen_file) as g:
     slines = s.readlines()
-   
-    for sline in slines:
-        sline = sline.strip().split()
-        words = set(sline)
-        sel_words = ", ".join([ f"{x} -> {y}" for x,y in vmap.items() if x in words ])
+    glines = g.readlines()
+    ilines = ip.readlines()
+    for sid, sline in enumerate(slines):
+        sline = sline.strip().split(" ")
+        ipline = ilines[sid].strip()
+        gline = glines[sid].strip()
+        #print(sline)
+        #print(gline)
+        #print(tups.findall(gline))
+        templates = [x.strip().split() for x in tups.findall(gline)]
+        #print(templates)
         
-        print(f"{' '.join(sline)} || {sel_words}")
+        current_tpl = templates[0]
+        ctpl_idx = 0
+        tidx = 0
+        mask_map = {}
+        for widx in range(len(sline)):
+            w = sline[widx]
+            if tidx > len(current_tpl) - 1:
+                tidx = 0
+                current_tpl = templates[ctpl_idx + 1]
+                ctpl_idx += 1
+            
+            tw = current_tpl[tidx]
+            if tw.startswith("W_"):
+                if tw not in mask_map:
+                    mask_map[tw] = None
+                mask_map[tw] = w
+                
+            tidx += 1
+                
+        for k, v in mask_map.items():
+            if mode == "all":
+                print(f"IN: {ipline} || OUT: {gline} || W: {k} || V: {v}")
+            elif mode == "input":
+                print(f"IN: {ipline} || OUT: {gline} || W: {k}")
+            elif mode == "output":
+                print(f"V: {v}")
+        
+        
+        
+        
